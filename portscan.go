0b5e085fe34a0bb2d290a102890d0dacfdd8d79d
intercept9 <ethancruize@gmail.com>
2016-11-16 12:50:07 -0800

    Add files via upload
    portscanner

---
+++ portscan.go
@@ -0,0 +1,51 @@
+//
+// All rights reserved. This program is free software. It comes without
+// any warranty, to the extent permitted by applicable law. You can
+// redistribute it and/or modify it under the terms of the Do What
+// The Fuck You Want To Public License, Version 2, as published by
+// Intercept9. 
+//
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+//
+
+package main
+
+import (
+        //"log"
+        "net"
+        "fmt"
+        "os"
+        "strconv"
+        )
+
+func main() {
+  callme()          
+ }
+
+func callme(){
+
+  var status string
+  getip := os.Args[1]
+  getport := 0
+       
+for i := 20; i < 80; i++ {
+  getport = i
+  data := getip+":"+strconv.Itoa(getport)
+  conn, err := net.Dial("tcp", data)
+   
+if err != nil {
+  //log.Println("Connection error:", err)     //When want a specific reason why it dint connect
+  status = strconv.Itoa(getport)+" - "+"Closed"
+} else {
+
+  status = strconv.Itoa(getport)+" - "+"Open"
+  defer conn.Close()
+}
+
+ fmt.Println(status)
+}
+}
